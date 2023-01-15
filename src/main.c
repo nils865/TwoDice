8fe3dca00caaa18d2d49d2510feb500e766270f6
Nils <nils.staas0806@gmail.com>
2023-01-15 10:52:41 +0100

    implemented random number generation

--- src/main.c
+++ src/main.c
@@ -2,5 +2,25 @@
 #include <stdlib.h>
 
 int main() {
-    printf("initial commit\n");
+    long count = 10000000;
+
+    long numbers[11];
+    float percentage[11];
+
+    for (size_t i = 0; i < (sizeof(numbers) / sizeof(numbers[0])); i++)
+        numbers[i] = 0;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        int num = ((rand() % 6) + 1) + ((rand() % 6) + 1);
+        numbers[num - 2]++;
+    }
+
+    for (size_t i = 0; i < (sizeof(percentage) / sizeof(percentage[0])); i++) 
+        percentage[i] = ((float)numbers[i] / (float)count) * 100;
+
+    for (size_t i = 0; i < (sizeof(numbers) / sizeof(numbers[0])); i++)
+        printf("%ld: %ld, %f %\n", (i + 2), numbers[i], percentage[i]);
+    
+    return 0;
 }
